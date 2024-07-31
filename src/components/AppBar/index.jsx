ea48c865d425aa2fed9f10ae6eacce3edc137633
nhnv13122001 <nhnv13122001@gmail.com>
2024-07-31 21:29:51 +0700

    Code Basic Layout

---
+++ src/components/AppBar/index.jsx
@@ -0,0 +1,21 @@
+import Box from '@mui/material/Box'
+
+import ModeSelect from '../ModeSelect'
+
+function AppBar() {
+  return (
+    <Box
+      sx={{
+        backgroundColor: 'primary.100',
+        width: '100%',
+        height: (theme) => theme.trello.appBarHeight,
+        display: 'flex',
+        alignItems: 'center'
+      }}
+    >
+      <ModeSelect />
+    </Box>
+  )
+}
+
+export default AppBar
